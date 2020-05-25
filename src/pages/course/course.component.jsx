c5c24ca24ad8de78b709a05bedfcfe1e83883e34
Mehdi Aghighi <mamaliosezar@gmail.com>
2020-05-26 00:26:38 +0430

    completed payment section

--- src/pages/course/course.component.jsx
+++ src/pages/course/course.component.jsx
@@ -2,48 +2,51 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import CourseIntro from "../../components/course-intro/course-intro.component";
-import CourseInfo from "../../components/course-info/course-info.component";
+// import CourseInfo from "../../components/course-info/course-info.component"
+import CustomLoader from "../../components/custom-loader/custom-loader.component";
 import CourseSections from "../../components/course-sections/course-sections.component";
 import RelatedCourses from "../../components/related-courses/related-courses.component";
 import API from "../../helpers/api";
 import { toast } from "react-toastify";
 
 function Course(props) {
-    let { slug } = useParams();
-    const [course, setCourse] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
+   let { slug } = useParams();
+   const [course, setCourse] = useState({});
+   const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        API.get(`/course/${slug}`)
-            .then((response) => {
-                return response.data.course;
-            })
-            .then((course) => {
-                setCourse(course);
-                setIsLoading(false);
-            })
-            .catch((err) => {
-                if (err.response) {
-                    toast.error(err.response.data.message);
-                } else {
-                    toast.error("دریافت اطلاعات دوره موفقیت‌آمیز نبود");
-                }
-            });
-    }, [slug]);
-    return (
-        <div>
-            {isLoading ? null : (
-                <>
-                    <CourseIntro course={course} />
-                    {/* <CourseInfo course={course} /> */}
-                    <CourseSections
-                        course={{ id: course.id, title: course.title }}
-                    />
-                    <RelatedCourses course={{ id: course.id }} />
-                </>
-            )}
-        </div>
-    );
+   useEffect(() => {
+      API.get(`/course/${slug}`)
+         .then((response) => {
+            return response.data.course;
+         })
+         .then((course) => {
+            setCourse(course);
+            setIsLoading(false);
+         })
+         .catch((err) => {
+            if (err.response) {
+               toast.error(err.response.data.message);
+            } else {
+               toast.error("دریافت اطلاعات دوره موفقیت‌آمیز نبود");
+            }
+         });
+   }, [slug]);
+   return (
+      <div>
+         {isLoading ? (
+            <CustomLoader />
+         ) : (
+            <>
+               <CourseIntro course={course} />
+               {/* <CourseInfo course={course} /> */}
+               <CourseSections
+                  course={{ id: course.id, title: course.title }}
+               />
+               <RelatedCourses course={{ id: course.id }} />
+            </>
+         )}
+      </div>
+   );
 }
 
 export default Course;
--- src/routes.js
+++ src/routes.js
@@ -3,41 +3,46 @@ import Blog from "./pages/blog/blog.component";
 import Post from "./pages/post/post.component";
 import Course from "./pages/course/course.component";
 import Courses from "./pages/courses/courses.component";
-import NotFound from "./pages/not-found/not-found.component";
+import Payment from "./pages/payment/payment.component";
 import Logout from "./pages/logout/logout.component";
+import NotFound from "./pages/not-found/not-found.component";
 
 const routes = [
-    {
-        path: "/",
-        component: Index,
-        exact: true,
-    },
-    {
-        path: "/blog",
-        component: Blog,
-        exact: true,
-    },
-    {
-        path: "/blog/:slug",
-        component: Post,
-    },
-    {
-        path: "/courses",
-        component: Courses,
-        exact: true,
-    },
-    {
-        path: "/course/:slug",
-        component: Course,
-    },
-    {
-        path: "/logout",
-        component: Logout,
-    },
-    {
-        component: NotFound,
-        status: 404,
-    },
+   {
+      path: "/",
+      component: Index,
+      exact: true,
+   },
+   {
+      path: "/blog",
+      component: Blog,
+      exact: true,
+   },
+   {
+      path: "/blog/:slug",
+      component: Post,
+   },
+   {
+      path: "/courses",
+      component: Courses,
+      exact: true,
+   },
+   {
+      path: "/course/:slug",
+      component: Course,
+   },
+   {
+      path: "/payment",
+      component: Payment,
+   },
+   {
+      path: "/logout",
+      component: Logout,
+   },
+   {
+      component: NotFound,
+      status: 404,
+   },
 ];
 
 export default routes;
