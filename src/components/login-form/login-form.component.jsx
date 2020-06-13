d3c3563426e476721df377410d707aaa8b2b4246
MehdiAghighi <mamaliosezar@gmail.com>
2020-06-14 01:29:43 +0430

    changed the cookie settings

--- src/components/login-form/login-form.component.jsx
+++ src/components/login-form/login-form.component.jsx
@@ -102,9 +102,12 @@ function LoginForm(props) {
             // localStorage.setItem("token", data.token)
             var expiryDate = new Date()
             expiryDate.setMonth(expiryDate.getMonth() + 1)
-            setCookie("token", data.token, { expires: expiryDate, path: "/" })
-            setCookie("token", data.token, {
-              domain: `${process.env.REACT_APP_DOMAIN}`,
+            // setCookie("token", `Bearer ${data.token}`, {
+            //   expires: expiryDate,
+            //   path: "/",
+            // })
+            setCookie("token", `Bearer ${data.token}`, {
+              domain: `.${process.env.REACT_APP_DOMAIN}`,
               httpOnly: true,
               expires: expiryDate,
               path: "/",
