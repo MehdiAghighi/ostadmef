c57e28af6153c59322c897a420c7863bdaf50c0d
Mehdi Aghighi <mamaliosezar@gmail.com>
2020-07-05 22:32:19 +0430

    corrected the titles

--- src/pages/courses/courses.component.jsx
+++ src/pages/courses/courses.component.jsx
@@ -29,7 +29,7 @@ function Courses(props) {
    return (
       <div className="container mx-auto">
          <Helmet>
-            <title>لینوم | دوره‌ها</title>
+            <title>لینوم | دوره‌های آموزشی کپسولی‌شده</title>
             <meta
                name="description"
               content="لینوم یک پلتفرم آموزشی میکرولرنینگ است که با تکیه بر ویدئو های کپسولی و فشرده ، یادگیری دروس دانشگاهی را در سریع ترین زمان ممکن به ارمغان می آورد."
